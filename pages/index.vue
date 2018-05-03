032b89c1d8c42bdfa64453e9cbaccf4ffbcbb1f1
Kubtuntha <buriramit@gmail.com>
2018-05-03 15:12:39 +0700

    edit-Page

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <img src="/LOGO_VEC_OK.png" alt="ระบบจัดการข้อมูลนักศึกษา" class="mb-5" />
+        <h1>ระบบจัดการข้อมูล นักเรียน นักศึกษา</h1>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id: 1,
+    }
+  },
+}
+</script>
